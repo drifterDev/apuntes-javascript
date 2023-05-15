4e4126b6f8a45c02c8bc079b2b7425853d275eab
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-05-14 19:34:21 -0500

    Continuando con JS

---
+++ aprendiendo/alertas y ventanas/alertas.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// alerta
+alert("Hola, soy una alerta");
+alert("Soy otra alerta");
+// Confirm es solo un aceptar o cancelar
+let resultado = confirm("Desea salirse?");
+console.log(resultado);
+
+//Input, valor por defecto
+let resultado2 = prompt("Que edad tienes?", 18);
+console.log(typeof resultado2, resultado2);
---
+++ aprendiendo/bucles/bucles.js
@@ -0,0 +1,11 @@
+"use strict";
+
+for (let i = 0; i < 100; i++) {
+  console.log(i + 1);
+}
+
+let n = 1;
+while (n * n < 1000) {
+  console.log(n * n);
+  n++;
+}
