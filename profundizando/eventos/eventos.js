46c105004e1d8db9ac3f8754c1d22f7542412fe8
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-05-17 18:23:03 -0500

    Eventos y timers

--- profundizando/eventos/eventos.js
+++ profundizando/eventos/eventos.js
@@ -4,6 +4,28 @@
 let boton = document.getElementById("boton");
 let caja = document.getElementById("caja");
 
-function cambiarColor(color) {
-  boton.style.background = color;
+function cambiarColor() {
+  console.log("1");
+  let color = boton.style.background;
+  if (color == "red") {
+    boton.style.background = "green";
+  } else if (color == "green") {
+    boton.style.background = "blue";
+  } else {
+    boton.style.background = "red";
+  }
+  return true;
 }
+
+//Añade un escuchador de eventos a cualquier objeto del html
+boton.addEventListener("click", cambiarColor);
+
+//Mouse over
+boton.addEventListener("mouseover", function () {
+  boton.style.background = "black";
+});
+
+//Mouse out
+boton.addEventListener("mouseout", function () {
+  boton.style.background = "white";
+});
---
+++ profundizando/eventos/timers.js
@@ -0,0 +1,45 @@
+"use strict";
+
+window.addEventListener("load", function () {
+  let boton = document.getElementById("boton");
+
+  //Timers
+
+  //Los setInterval se ejecutan multiples veces
+  //Se define la funcion y luego se especifica el tiempo en milisegundos
+  function intervalo() {
+    let tiempo = setInterval(function () {
+      console.log("Hola soy setInterval");
+      if (boton.style.background == "red") {
+        boton.style.background = "blue";
+      } else {
+        boton.style.background = "red";
+      }
+    }, 3000);
+    return tiempo;
+  }
+
+  //Los setTimeOut solo se ejecuta una sola vez
+  let tiempo2 = setTimeout(function () {
+    console.log("Hola soy setTimeOut");
+    if (boton.style.background == "red") {
+      boton.style.background = "blue";
+    } else {
+      boton.style.background = "red";
+    }
+  }, 4000);
+
+  let tiempo = intervalo();
+
+  //Para eliminar el intervalo
+  let stop = document.getElementById("stop");
+  stop.addEventListener("click", function () {
+    clearInterval(tiempo);
+  });
+
+  //Para comenzar el intervalo
+  let start = document.getElementById("start");
+  start.addEventListener("click", function () {
+    intervalo();
+  });
+});
