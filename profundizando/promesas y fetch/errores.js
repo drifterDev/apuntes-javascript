bcf23d1016a357dd8dc6455e0592b6a4966c4dda
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-05-22 15:40:56 -0500

    Promesas y try-catch

---
+++ profundizando/promesas y fetch/errores.js
@@ -0,0 +1,11 @@
+"use strict";
+
+window.addEventListener("load", function () {
+  let year = 2004;
+  try {
+    console.log(ye);
+    console.log("Sigue ejecutando el bloque try?"); //No
+  } catch (error) {
+    alert(error);
+  }
+});
