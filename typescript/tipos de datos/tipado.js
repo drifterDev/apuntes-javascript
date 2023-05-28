96dcaed88375f403806d76be2cc1eb1eeb10f339
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-05-28 10:21:06 -0500

    Archivos nuevos

---
+++ typescript/tipos de datos/tipado.js
@@ -0,0 +1,34 @@
+//Tipos de datos
+var cadena = "Hola mundo!";
+var numero = 24;
+var boolean = true;
+var cualquiera = "Cualquier tipo de dato";
+var libros = ["El bestiario de axlin", "Los heroes del olimpo"];
+var coleccion = ["Hola", 24, false];
+console.log(cadena);
+console.log(numero);
+console.log(boolean);
+console.log(cualquiera);
+console.log(libros);
+console.log(coleccion);
+//Tipos de datos multiples
+// var variable: number | string = "Hola";
+var variable = "Hola";
+console.log(variable);
+variable = 24;
+console.log(variable);
+//La diferencia que hay entre let y var
+//Var es global y let es en bloque
+var n1 = 24;
+var n2 = 18;
+if (n1 == 24) {
+    var n1_1 = 42;
+    var n2 = 20;
+    console.log(n1_1, n2);
+}
+console.log(n1, n2);
+//Funciones y tipado
+function suma(n1, n2, mensaje) {
+    return mensaje + (n1 + n2);
+}
+console.log(suma(15, 12, "La suma es: "));
