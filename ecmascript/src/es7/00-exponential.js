77e2f1bee9dcb0af4230c08b33794a584a78b34b
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-06-06 08:13:24 -0500

    Ecmascript es7

---
+++ ecmascript/src/es7/00-exponential.js
@@ -0,0 +1,5 @@
+const data = 3 ** 4;
+console.log(data);
+
+const data2 = 34 ** 4;
+console.log(data2);
---
+++ ecmascript/src/es7/01-array-includes.js
@@ -0,0 +1,5 @@
+let nums = [1, 2, 3, 4, 5, 6, 67, 567];
+console.log(nums.includes(6));
+console.log(nums.includes(7));
+console.log(nums.includes(67));
+console.log(nums.includes(68));
