f29d0d662ade7eaf0b4c766c886a46affa474764
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-06-27 23:38:41 -0500

    Editando el canvas

--- videojuego-canvas/script.js
+++ videojuego-canvas/script.js
@@ -1,8 +1,21 @@
 "use strict";
 const canvas = document.getElementById("canvas");
 const game = canvas.getContext("2d");
+let medida;
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
-window.addEventListener("load", startGame);
+function setCanvasSize() {
+  let altura = window.innerHeight;
+  let ancho = window.innerWidth;
+  medida = Math.min(altura * 0.7, ancho * 0.7);
+  canvas.setAttribute("width", medida);
+  canvas.setAttribute("height", medida);
+
+  //Lo invocamos así porque cada vez que se
+  //cambia el tamaño del canvas su contenido se borra
+  startGame();
+}
 
 function startGame() {
   //Metodos para rellenar el canvas
@@ -12,15 +25,17 @@ function startGame() {
   //game.font="tamaño font-fmaily"
   //game.fillstyle='red'
   //game.textAlign='center'
-
   //Metodo para manejar el canvas
-  let altura = window.innerHeight;
-  let ancho = window.innerWidth;
-  let medida = Math.min(altura * 0.7, ancho * 0.7);
-  canvas.setAttribute("width", medida);
-  canvas.setAttribute("height", medida);
-
-  let elementSize = medida / 10;
+  // let altura = window.innerHeight;
+  // let ancho = window.innerWidth;
+  // let medida = Math.min(altura * 0.7, ancho * 0.7);
+  // canvas.setAttribute("width", medida);
+  // canvas.setAttribute("height", medida);
+  let elementSize = medida / 10 - 1;
   game.font = elementSize + "px Verdana";
-  game.fillText(emojis["X"], 100, 100);
+  for (let i = 0; i < 10; i++) {
+    game.fillText(emojis["X"], i * elementSize, elementSize);
+  }
+  //Canvas responsive
+  //Añadiendo el evento a la ventana resize
 }
