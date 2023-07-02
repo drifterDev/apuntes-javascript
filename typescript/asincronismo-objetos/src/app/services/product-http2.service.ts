be270579bf9222d039cf34540ca6289634373a01
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-07-01 23:26:17 -0500

    Genericos en metodos y clases

---
+++ typescript/asincronismo-objetos/src/app/services/product-http2.service.ts
@@ -0,0 +1,8 @@
+import { Product } from '../models/product.model';
+import { BaseHttpService } from './base-http.service';
+
+export class ProductHttpService extends BaseHttpService<Product> {
+  otroRequest() {
+    this.url;
+  }
+}
