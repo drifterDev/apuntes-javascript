a922bed8ffa86ca1ff2865545e7b0a53094aa3c8
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-08-02 23:06:45 -0500

    Proyecto #6 con tailwind

--- frontend/proyecto6/tailwind.config.js
+++ frontend/proyecto6/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purpleP: "#7441C7",
+        grayP: "#48556A",
+        blackP: "#19202E",
+        whiteP: "#FFFFFF",
+        lightP: "#CECECE",
+        grayish: "#ECF2F8",
+      },
+    },
   },
   plugins: [],
 };
