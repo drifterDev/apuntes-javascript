828f3e707b73cefd6e864f93bf23033a87c21113
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-08-19 09:17:21 -0500

    Haciendo pruebas e2e e integración y ui

---
+++ pruebas-testing/api/e2e/books.e2e.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+const { MongoClient } = require('mongodb');
+
+const createApp = require('../src/app');
+const { config } = require('../src/config');
+
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
+
+describe('Test for books', () => {
+  let app = null;
+  let server = null;
+  let database = null;
+
+  beforeAll(async () => {
+    app = createApp();
+    server = app.listen(3001);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
+  });
+
+  afterAll(async () => {
+    await server.close();
+    await database.dropDatabase();
+  });
+
+  describe('test for [GET] /api/v1/books', () => {
+    test('should return a list books', async () => {
+      // Arrange
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'Book1',
+          year: 2004,
+          author: 'mateo',
+        },
+        {
+          name: 'Book2',
+          year: 2004,
+          author: 'mateo',
+        },
+      ]);
+      console.log(seedData);
+      // Act
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          // Assert
+          expect(body.length).toEqual(seedData.insertedCount);
+        });
+    });
+  });
+});
