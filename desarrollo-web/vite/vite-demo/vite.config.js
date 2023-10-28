c214c0dcd4b1ba9b408b55899e3789528eeeb811
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-10-28 18:40:29 -0500

    Configuración básica

--- desarrollo-web/vite/vite-demo/vite.config.js
+++ desarrollo-web/vite/vite-demo/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig  } from "vite"
+import { defineConfig, loadEnv  } from "vite"
 
-export default defineConfig({
-  server: {
-    port: 8080
+export default defineConfig(({ command, mode }) => {
+  let port = 3000
+
+  // Tienen que ser así VITE_nombre
+  const env = loadEnv(mode, process.cwd())
+  console.log(env)
+
+  console.log(command, mode)
+  if (mode === 'development') {
+    port = 8080
+    console.log('Estoy en modo DEV')
+  } else {
+    console.log('Estoy en modo BUILD')
+  }
+
+  return {
+    server: {
+      port
+    }
   }
 })
