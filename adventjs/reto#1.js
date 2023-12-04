2344dd7f11effc80220fd9370daadd76aa458714
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-12-04 12:09:05 -0500

    Comenzando con adventjs

---
+++ adventjs/reto#1.js
@@ -0,0 +1,32 @@
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+function findFirstRepeated(gifts) {
+  let ans = -1
+  let array = []
+  gifts.forEach(element => {
+    if(array.includes(element) && ans == -1){
+      ans = element
+    }
+    array.push(element)
+  })
+  return ans
+}
+
+const giftIds = [2, 1, 3, 5, 3, 2]
+const firstRepeatedId = findFirstRepeated(giftIds)
+console.log(firstRepeatedId) // 3
+// Aunque el 2 y el 3 se repiten
+// el 3 aparece primero por segunda vez
+
+const giftIds2 = [1, 2, 3, 4]
+const firstRepeatedId2 = findFirstRepeated(giftIds2)
+console.log(firstRepeatedId2) // -1
+// Es -1 ya que no se repite ningún número
+
+const giftIds3 = [5, 1, 5, 1]
+const firstRepeatedId3 = findFirstRepeated(giftIds3)
+console.log(firstRepeatedId3) // 5
